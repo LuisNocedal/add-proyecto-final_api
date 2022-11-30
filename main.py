095fa46ffbe8d60e3043617ddce6ed6714ca350d
LuisNocedal <luisnocedal1119@gmail.com>
2022-11-30 17:56:50 -0600

    Login

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ async def root():
         ["2016", 660, 1120, 300],
         ["2017", 1030, 540, 350],
     ]}
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+@app.post('/login')
+async def login(login: Login):
+    if login.username == "user1" and login.password == "1234":
+        return {"status": True, "token": "12345678"}
+    else:
+        return {"status": False}
