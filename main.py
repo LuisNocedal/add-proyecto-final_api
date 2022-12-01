e08d26a299081bb1072a178ba0cf4e6846751d2e
Mariam251299 <mariam.delgado4258@alumnos.udg.mx>
2022-11-30 18:25:14 -0600

    Cambios

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import mysql.connector
 import json
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # conn = mysql.connector.connect(user='root', password='root', host='localhost', database='sakila', auth_plugin='mysql_native_password')
 # cursor = conn.cursor()
 
